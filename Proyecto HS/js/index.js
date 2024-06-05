bffaf04ac5b38fc876ccd6dd192597e2d862d134
Matías Álvarez L. <alvarezlmatias@gmail.com>
2024-06-04 23:58:34 -0300

    Realizándole los últimos ajustes al proyecto

--- Proyecto HS/js/index.js
+++ Proyecto HS/js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', () => {
 
 // Variables
@@ -284,24 +283,4 @@ DOMbotonVaciar.addEventListener('click', vaciarCarrito);
 cargarCarritoDeLocalStorage();
 renderizarProductos();
 renderizarCarrito();
-=======
-const user = JSON.parse(localStorage.getItem('login_success')) || false;
-if(!user){
-    window.location.href = '../pages/login.html'
-};
-
-const logout = document.querySelector('#logout');
-
-logout.addEventListener('click', ()=>{
-    Swal.fire({
-        title: '¡Hasta la próxima!',
-        text: 'Gracias por visitar nuestro sitio',
-        icon: 'success',
-        iconColor: 'green',
-        confirmButtonText: 'Cerrar sesión'
-    }).then((result)=>{
-        window.location.href = './pages/login.html';
-        localStorage.removeItem('login_success')
-    });
->>>>>>> 4c725802e56e4a3bff8208662bd8ad6ec697cef6
 })
